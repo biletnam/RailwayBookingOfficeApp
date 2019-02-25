98791f42274560051922a425a804e563577d6de0
Vladymir <vladymirsheyko@gmail.com>
2019-02-25 22:51:27 +0200

    changed GepericRepo

--- src/main/java/GenericRepo.java
+++ src/main/java/GenericRepo.java
@@ -4,7 +4,6 @@ import java.util.List;
 public interface GenericRepo<T,ID> {
     void save(T t) throws IOException;
     List<T> findAll() throws IOException;
-    void update(T t);
     void delete(ID id) throws IOException;
     T getById(ID id) throws IOException;
 
